72ca5e88e00d62259f0d31629ea1dcc422195661
Nishant5789 <94158059+Nishant5789@users.noreply.github.com>
2022-06-07 13:09:22 +0530

    Update team.php

--- team.php
+++ team.php
@@ -33,11 +33,11 @@
             <div class="col-xl-3 col-sm-6 my-5 mx-auto">
                 <div class="bg-light rounded shadow-sm py-5 px-4"><img src="assets/nishant.jpeg" alt="" width="150" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
                     <h5 class="mb-0">Nishant Bhandari</h5><span class="small text-uppercase text-muted">Frontend</span>
-                    <ul class="social mb-0 list-inline mt-3">   
-                        <li class="list-inline-item"><a href="https://github.com/Nishant5789" target="_blank" ><i class="fa-brands fa-github"></i></a></li>
+                  <ul class="social mb-0 list-inline mt-3">   
+                        <li class="list-inline-item"><a href="https://github.com/Nishant5789" ><i class="fa-brands fa-github"></i></a></li>
                         <!-- <li class="list-inline-item"><a href="#" ><i class="fa-brands fa-twitter"></i></a></li> -->
-                        <li class="list-inline-item"><a href="https://www.linkedin.com/in/nishant-bhandari-193a83201/"  target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
-                        <li class="list-inline-item"><a href="https://www.instagram.com/nishantbhandari1399/" target="_blank"  ><i class="fa-brands fa-instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="#" ><i class="fa-brands fa-linkedin"></i></a></li>
+                        <li class="list-inline-item"><a href="https://www.instagram.com/nishantbhandari1399/" ><i class="fa-brands fa-instagram"></i></a></li>
                     </ul>
                 </div>
             </div><!-- End -->
